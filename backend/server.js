2e27837b61341181d56ecf16961ba84c48628bd0
pastorAlexanderCastro <pastorcastro237@gmail.com>
2026-06-11 07:57:17 -0400

    Terminado el rediseño de la página de inicio

--- backend/server.js
+++ backend/server.js
@@ -10,53 +10,65 @@ app.use(express.json());
 
 // Configuración de conexión
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "contactos_db"
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "contactos_db",
 });
 
 // Conectar a MySQL
 db.connect((err) => {
-    if (err) {
-        console.error("Error de conexión:", err);
-    } else {
-        console.log("Conectado a MySQL");
-    }
+  if (err) {
+    console.error("Error de conexión:", err);
+  } else {
+    console.log("Conectado a MySQL");
+  }
 });
 
 // Ruta de prueba
 app.get("/", (req, res) => {
-    res.send("Servidor conectado a MySQL");
+  res.send("Servidor conectado a MySQL");
 });
-
 
 // ✅ RUTA PARA GUARDAR DATOS
 app.post("/guardar", (req, res) => {
+  const { nombre, correo, mensaje } = req.body;
 
-    const { nombre, correo, mensaje } = req.body;
+  console.log("Datos recibidos:", req.body);
 
-    console.log("Datos recibidos:", req.body);
+  if (!nombre || !correo || !mensaje) {
+    return res.status(400).send("Datos incompletos");
+  }
 
-    if (!nombre || !correo || !mensaje) {
-        return res.status(400).send("Datos incompletos");
+  const sql =
+    "INSERT INTO contactos (nombre, correo, mensaje) VALUES (?, ?, ?)";
+
+  db.query(sql, [nombre, correo, mensaje], (err, result) => {
+    if (err) {
+      console.error("Error SQL:", err);
+      return res.status(500).send("Error en servidor");
     }
 
-    const sql = "INSERT INTO contactos (nombre, correo, mensaje) VALUES (?, ?, ?)";
-
-    db.query(sql, [nombre, correo, mensaje], (err, result) => {
-        if (err) {
-            console.error("Error SQL:", err);
-            return res.status(500).send("Error en servidor");
-        }
-
-        console.log("Registro insertado:", result);
-        res.send("Datos guardados correctamente");
-    });
+    console.log("Registro insertado:", result);
+    res.send("Datos guardados correctamente");
+  });
 });
 
+app.get("/productos", (req, res) => {
+  // Le pedimos a MySQL que traiga todo de la tabla productos
+  const sql = "SELECT * FROM productos";
+
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.error("Error al consultar:", err);
+      return res.status(500).send("Error al obtener datos");
+    }
+    // Si todo sale bien, enviamos los datos en formato JSON
+    res.json(results);
+  });
+});
 
 // Iniciar servidor
 app.listen(3000, () => {
-    console.log("Servidor en http://localhost:3000");
+  console.log("Servidor en http://localhost:3000");
 });
