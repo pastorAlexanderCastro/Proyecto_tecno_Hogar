e05652560251a4c8c8b4dbfd2a8f915aa7a6982a
pastorAlexanderCastro <pastorcastro237@gmail.com>
2026-06-17 14:55:25 -0400

    Se actualiza el formato de las fotos de jpg a webp

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,25 +1,12 @@
-const { arch } = require("node:os");
+//const { arch } = require("node:os");
 
-function agrgarNav() {
-  const nav = document.getElementById("nav");
-  const li = document.createElement("li");
-  li.className = "nav-item";
-  li.innerHTML = `<a class=" nav-link" href="admin-productos.html">Administración</a>`;
-  nav.appendChild(li);
+const usuario = localStorage.getItem("usuario");
+
+function ValidarSeccion() {
+  if (!usuario) {
+    window.location.href = "login.html";
+    alert("Debe iniciar sesión para acceder al panel administrativo");
+  }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  const usuario = localStorage.getItem("usuario");
-  if (usuario) {
-    agrgarNav();
-  } else {
-    alert("Debe iniciar sesión para acceder al panel administrativo");
-    window.location.href = "login.html";
-  }
-});
-const botonCerrarSeccion = document.getElementById("cerrar-seccion");
-console.log(botonCerrarSeccion);
-
-botonCerrarSeccion.addEventListener("click", () => {
-  alert(todobien);
-});
+ValidarSeccion(usuario);
--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,50 +1,61 @@
 const formLogin = document.getElementById("formLogin");
 const mensajeLogin = document.getElementById("mensajeLogin");
 
+if (formLogin) {
+  formLogin.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const correo = document.getElementById("correo");
+    const password = document.getElementById("password");
+    const nombre = document.getElementById("nombre");
 
+    try {
+      const respuesta = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          correo: correo.value,
+          password: password.value,
+        }),
+      });
+      const datos = await respuesta.json();
+      if (datos.ok) {
+        localStorage.setItem("usuario", JSON.stringify(datos.usuario));
+        mensajeLogin.innerHTML = `<div class="alert alert-success">${datos.mensaje}</div>`;
+        nombre.classList.add("is-valid");
+        correo.classList.add("is-valid");
+        password.classList.add("is-valid");
 
-formLogin.addEventListener("submit", async function (event) {
-  event.preventDefault();
-  const correo = document.getElementById("correo");
-  const password = document.getElementById("password");
-  const nombre = document.getElementById("nombre");
+        setTimeout(() => {
+          window.location.href = "admin-productos.html";
+          agrgarNav();
+        }, 1000);
+      } else {
+        mensajeLogin.innerHTML = `<div class="alert alert-danger">${datos.mensaje}</div>`;
 
-  /* <li id="i" class="nav-item"><a class=" nav-link" href="admin-productos.html">Administración</a></li>*/
-
-  try {
-    const respuesta = await fetch("http://localhost:3000/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ correo: correo.value, password: password.value }),
-    });
-    const datos = await respuesta.json();
-    if (datos.ok) {
-      localStorage.setItem("usuario", JSON.stringify(datos.usuario));
-      mensajeLogin.innerHTML = `<div class="alert alert-success">${datos.mensaje}</div>`;
-      nombre.classList.add("is-valid");
-      correo.classList.add("is-valid");
-      password.classList.add("is-valid");
-
-      setTimeout(() => {
-        window.location.href = "admin-productos.html";
-        agrgarNav();
-      }, 1000);
-    } else {
-      mensajeLogin.innerHTML = `<div class="alert alert-danger">${datos.mensaje}</div>`;
-
-      nombre.classList.add("is-invalid");
-      correo.classList.add("is-invalid");
-      password.classList.add("is-invalid");
-      setTimeout(() => {
-        nombre.classList.remove("is-invalid");
-        correo.classList.remove("is-invalid");
-        password.classList.remove("is-invalid");
-        mensajeLogin.innerHTML = "";
-        formLogin.reset();
-      }, 3000);
+        nombre.classList.add("is-invalid");
+        correo.classList.add("is-invalid");
+        password.classList.add("is-invalid");
+        setTimeout(() => {
+          nombre.classList.remove("is-invalid");
+          correo.classList.remove("is-invalid");
+          password.classList.remove("is-invalid");
+          mensajeLogin.innerHTML = "";
+          formLogin.reset();
+        }, 3000);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      mensajeLogin.innerHTML = `<div class="alert alert-danger">No se pudo conectar con el servidor</div>`;
     }
-  } catch (error) {
-    console.error("Error:", error);
-    mensajeLogin.innerHTML = `<div class="alert alert-danger">No se pudo conectar con el servidor</div>`;
-  }
-});
+  });
+}
+
+const botonCerrarSeccion = document.getElementById("cerrar-seccion");
+
+if (botonCerrarSeccion) {
+  botonCerrarSeccion.addEventListener("click", () => {
+    localStorage.removeItem("usuario");
+    confirm("Seguro que quiere cerrar seccion");
+    window.location.href = "login.html";
+  });
+}
